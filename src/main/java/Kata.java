baf8b67df3d0f9788196b011942a293de0efe2de
superexplorador <orurojm@gmail.com>
2019-01-09 19:47:18 -0400

    por fin jejej

---
+++ src/main/java/Kata.java
@@ -0,0 +1,43 @@
+import java.text.DecimalFormat;
+/**
+ * In this little assignment you are given a string
+ * of space separated numbers,
+ * and have to return the highest and lowest number.
+ * HighAndLow("1 2 3 4 5")  // return "5 1"
+ * HighAndLow("1 2 -3 4 5") // return "5 -3"
+ * HighAndLow("1 9 3 4 -5") // return "9 -5".
+ */
+
+public final class Kata {
+    /**
+     * es un constructor vacio.
+     *
+     */
+    private Kata() {
+    }
+    /**
+     * Metodo para sacar numeros altos y numeros bajos.
+     *
+     * @param numbers . En este parametro se ingresa una cadena
+     *                separado por espacios.
+     * @return
+     *          Devuelve el mayor y el menos Highest and lowest.
+     *
+     */
+
+    public static String highAndLow(final String numbers) {
+        String[] cadena = numbers.split(" ");
+        float mayor = Float.parseFloat(cadena[0]);
+        float menor = Float.parseFloat(cadena[0]);
+        for (int i = 1; i <= (cadena.length - 1); ++i) {
+            if (Float.parseFloat(cadena[i]) > mayor) {
+                mayor = Float.parseFloat(cadena[i]);
+            }
+            if (Float.parseFloat(cadena[i]) < menor) {
+                menor = Float.parseFloat(cadena[i]);
+            }
+        }
+        DecimalFormat formato = new DecimalFormat("#");
+        return (formato.format(mayor)) + " " + formato.format(menor);
+    }
+}
--- src/test/java/KataTest.java
+++ src/test/java/KataTest.java
@@ -1,17 +1,20 @@
 import org.junit.Test;
-import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
-public class kataTest {
+/**
+ * Este es una clase de prueba.
+ */
+public class KataTest {
     /**
+     * primera frase jejeje.
      *  final int expected = 6541;
      *         final int num = 1654;
      *         assertEquals(expected, Kata1.sortDesc(num));
      */
     @Test
-    public void Test() {
+    public void test() {
       //  System.out.println("hola "+kata.HighAndLow("-7 3 43 2 0"));
 
-       assertEquals("42 -9", kata.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+       assertEquals("42 -9", Kata.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
     }
 }
